2b0546591c68856c1321c7ef0c89db7a9f501dcb
fridah45al <fridaheyanae146@gmail.com>
2026-06-22 13:57:10 +0300

    Add files via upload

---
+++ assessment 5.js
@@ -0,0 +1,16 @@
+let edible=["Bread","crips"];
+let nonEdible=["plastic cups"];
+let machinery=["Motorcycle","Electric Drill","electic Saw"];
+//ordered output
+console.log("Edible Goods:");
+console.log(edible);
+console.log("Non-Edible Goods:");
+console.log(nonEdible);
+console.log("Machinery:");
+console.log(machinery);
+document.write("<h2>Edible Goods:</h2>");
+document.write(edible+"<br>");
+document.write("<h2>Non-Edible Goods:</h2>");
+document.write(nonEdible+"<br>");
+document.write("<h2>Machinery:</h2>");
+document.write(machinery);
